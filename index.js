d65dee4bd857e2d70c02f89a6a5339b0cda758fe
ROMEO JEIRFIMCZUK ALMEIDA <46583925@ort>
2022-08-11 10:14:38 -0300

    Third commit

--- index.js
+++ index.js
@@ -1,63 +1,129 @@
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
 
-canvas.width= 1024;
-canvas.height= 576;
+canvas.width = 1024;
+canvas.height = 576;
 
 c.fillRect(0, 0, canvas.width, canvas.height);
+const gravity = 0.2;
 
 class Sprite {
-    constructor( {position, speed,  } ){
+    constructor({ position, speed, }) {
         this.position = position;
         this.speed = speed;
+        this.height = 150
+        this.width = 50
+        this.topheight = -150
     }
-        draw() {
-            c.fillStyle= 'green'
-            c.fillRect(this.position.x, this.position.y, 50, 150);
+    draw() {
+        c.fillStyle = 'green'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height);
 
+    }
+
+    update() {
+
+        this.draw()
+
+        this.position.y += this.speed.y;
+        this.position.x += this.speed.x;
+
+        if (this.position.y + this.height + this.speed.y >= canvas.height) {
+            this.speed.y = 0;
+
+        } else {
+
+            this.speed.y += gravity;
         }
-
-        update(){
-
-            this.draw()
-            this.position.y += this.speed.y;
-        }
+    }
 }
 
 const player = new Sprite({
-    position:{
-        x:0,
-        y:0,
+    position: {
+        x: 0,
+        y: 0,
     },
-    speed:{
-        x:0,
-        y:10,
+    speed: {
+        x: 0,
+        y: 10,
     },
 
 })
 
 const enemy = new Sprite({
-    position:{
-        x:975,
-        y:0,
+    position: {
+        x: 975,
+        y: 0,
     },
-        speed:{
-        x:0,
-        y:0,
+    speed: {
+        x: 0,
+        y: 10,
     },
 })
 
-function animation(){
-    window.requestAnimationFrame(animation)
-    c.fillStyle= "black"
-    c.clearRect(0 ,0 , canvas.width, canvas.height)
+const keys = {
+    a:{
+        pressed: false
+    },
+    d:{
+        pressed: false
+    }
 
+}
+
+function animation() {
+    window.requestAnimationFrame(animation)
+    c.fillStyle = 'black';
+    c.fillRect(0, 0, canvas.width, canvas.height);
     player.update()
     enemy.update()
+
+    player.speed.x=0;
+    
+    if(keys.a.pressed){
+        player.speed.x = -2.5;
+    }else if(keys.d.pressed){
+        player.speed.x = 2.5;
+    }
 }
+
+window.addEventListener('keydown', (event) => {
+    switch (event.key) {
+        case 'd': keys.d.pressed = true;
+            break
+    }
+    switch (event.key) {
+        case 'w': player.speed.y = -10;
+            break
+    }
+    switch (event.key) {
+        case 'a': keys.a.pressed = true;
+            break
+    }
+})
+
+window.addEventListener('keyup', (event) => {
+    switch (event.key) {
+        case 'd': keys.d.pressed = false;
+            break
+    }
+    switch (event.key) {
+        case 'w': player.speed.y = 0;
+            break
+    }
+    switch (event.key) {
+        case 'a': keys.a.pressed = false;
+            break
+    }
+
+})
+
+
+
 
 player.draw()
 
 enemy.draw()
 
 animation()
+
